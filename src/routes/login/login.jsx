1e03579ee4c95d0a8e277015693f83aed4306d1e
Talat09 <talat181008312@gmail.com>
2024-11-06 16:18:08 +0600

    register and login api integration done

--- src/routes/login/login.jsx
+++ src/routes/login/login.jsx
@@ -1,14 +1,45 @@
+import toast, { Toaster } from "react-hot-toast";
 import "./login.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import axios from "axios";
 
 function Login() {
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+
+    const password = e.target.password.value;
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5000/api/V1/auth/login",
+        { username, password },
+        { withCredentials: true }
+      );
+      console.log("login response:", response.data);
+      localStorage.setItem("user", JSON.stringify(response.data));
+      if (response.status === 200) {
+        toast.success("Registration successful!");
+        navigate("/");
+      }
+    } catch (error) {
+      // console.error("New Error:", error.response.data.message);
+      setError(error.response.data.message);
+      toast.error("An error occurred: " + error.response.data.message);
+    }
+  };
   return (
     <div className="login">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleLogin}>
           <h1>Welcome back</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="password" type="password" placeholder="Password" />
+          <p style={{ color: "red" }}>{error}</p>
           <button>Login</button>
           <Link to="/register">{"Don't"} you have an account?</Link>
         </form>
--- src/routes/register/register.jsx
+++ src/routes/register/register.jsx
@@ -1,16 +1,79 @@
+import { useState } from "react";
 import "./register.scss";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 function Register() {
+  const [error, setError] = useState("");
+  const [showPassword, setShowPassword] = useState(false); // State to manage password visibility
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    const username = e.target.username.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    const user = {
+      username,
+      email,
+      password,
+    };
+
+    try {
+      const response = await fetch(
+        "http://localhost:5000/api/V1/auth/register",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(user),
+        }
+      );
+
+      if (response.ok) {
+        toast.success("Registration successful!");
+        navigate("/login");
+      } else {
+        const errorData = await response.json();
+        setError(errorData.message);
+        toast.error("An error occurred: " + errorData.message);
+      }
+    } catch (error) {
+      console.error("New Error:", error.response);
+    }
+  };
+
   return (
     <div className="register">
+      <Toaster position="top-center" reverseOrder={false} />
       <div className="formContainer">
-        <form>
+        <form onSubmit={handleRegister}>
           <h1>Create an Account</h1>
           <input name="username" type="text" placeholder="Username" />
           <input name="email" type="text" placeholder="Email" />
-          <input name="password" type="password" placeholder="Password" />
-          <button >Register</button>
+
+          <div
+            className="passwordContainer"
+            style={{ position: "relative", width: "100%" }}
+          >
+            <span
+              style={{ position: "absolute", right: "10px", top: "35%" }}
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ?<AiOutlineEye />  :<AiOutlineEyeInvisible />}
+            </span>
+            <input
+              style={{ width: "100%" }}
+              name="password"
+              type={showPassword ? "text" : "password"}
+              placeholder="Password"
+            />
+          </div>
+
+          <p style={{ color: "red" }}>{error}</p>
+          <button>Register</button>
           <Link to="/login">Do you have an account?</Link>
         </form>
       </div>
