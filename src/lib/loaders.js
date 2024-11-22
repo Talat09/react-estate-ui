cc77ee7c1a2bb1bdc46b22d86b637989c40040ce
Talat09 <talat181008312@gmail.com>
2024-11-22 15:10:40 +0600

    load list page search data

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -6,3 +6,10 @@ export const singlePageLoader = async ({ request, params }) => {
   );
   return res.data;
 };
+export const listPageLoader = async ({ request, params }) => {
+  const query = request.url.split("?")[1];
+  console.log("request: ", request);
+  const res = await axios.get(`http://localhost:5000/api/V1/posts?${query}`);
+  console.log("res: ", res);
+  return res.data;
+};
