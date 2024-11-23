f6c3bfa0b29f525663d806f7b2f6d9bf7380c639
Talat09 <talat181008312@gmail.com>
2024-11-23 17:37:05 +0600

    feat: added filter search in list route

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { defer } from "react-router-dom";
 
 export const singlePageLoader = async ({ request, params }) => {
   const res = await axios.get(
@@ -8,8 +9,10 @@ export const singlePageLoader = async ({ request, params }) => {
 };
 export const listPageLoader = async ({ request, params }) => {
   const query = request.url.split("?")[1];
-  console.log("request: ", request);
-  const res = await axios.get(`http://localhost:5000/api/V1/posts?${query}`);
-  console.log("res: ", res);
-  return res.data;
+
+  const postPromise = axios.get(`http://localhost:5000/api/V1/posts?${query}`);
+
+  return defer({
+    postResponse: postPromise,
+  });
 };
--- src/routes/listPage/listPage.jsx
+++ src/routes/listPage/listPage.jsx
@@ -2,23 +2,66 @@ import "./listPage.scss";
 import Filter from "../../components/filter/Filter";
 import Card from "../../components/card/Card";
 import Map from "../../components/map/Map";
-import { useLoaderData } from "react-router-dom";
+import { Await, useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
 
 function ListPage() {
-  const posts = useLoaderData();
-  console.log(posts);
+  const data = useLoaderData();
+  console.log(data);
   return (
     <div className="listPage">
       <div className="listContainer">
         <div className="wrapper">
           <Filter />
-          {posts.map((item) => (
-            <Card key={item.id} item={item} />
-          ))}
+          <Suspense
+            fallback={
+              <img
+                style={{
+                  width: "200px",
+                  display: "flex",
+                  margin: "auto",
+                }}
+                src="/loading.gif"
+                alt="loading"
+              />
+            }
+          >
+            <Await
+              resolve={data.postResponse}
+              errorElement={<p>Error loading posts!</p>}
+            >
+              {(postResponse) =>
+                postResponse.data.map((item) => (
+                  <Card key={item.id} item={item} />
+                ))
+              }
+            </Await>
+          </Suspense>
         </div>
       </div>
       <div className="mapContainer">
-        <Map items={posts} />
+        <Suspense
+          fallback={
+            <img
+              style={{
+                width: "200px",
+                display: "flex",
+                margin: "auto",
+              }}
+              src="/loading.gif"
+              alt="loading"
+            />
+          }
+        >
+          <Await
+            resolve={data.postResponse}
+            errorElement={<p>Error loading posts!</p>}
+          >
+            {(postResponse) => <Map items={postResponse.data} />}
+          </Await>
+        </Suspense>
+
+        {/* <Map items={data} /> */}
       </div>
     </div>
   );
